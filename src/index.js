347d50487594ce4a1b322d2f7594336d75261e25
daniel-a-r <daniel.a-r@outlook.com>
2025-01-14 04:49:15 -0500

    experiment with localStorage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+// if key 'projects' does not exist, create default item
+// else, print to console
+if (!localStorage.getItem('projects')) {
+  localStorage.setItem('projects', JSON.stringify(['project1', 'project2', 'project3']));
+} else {
+  const projects = JSON.parse(localStorage.getItem('projects'));
+  console.log(projects);
+  projects.forEach((project) => {
+    console.log(project);
+  });
+}
