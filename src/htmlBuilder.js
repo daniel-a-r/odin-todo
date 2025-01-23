7cd76660a04243bfa9b94ca15a05c155415df6fc
daniel-a-r <daniel.a-r@outlook.com>
2025-01-23 03:16:17 -0500

    add functions to create header

--- src/htmlBuilder.js
+++ src/htmlBuilder.js
@@ -1,5 +1,7 @@
 export const init = (projectTitleList) => {
-  createProjectButtonList(projectTitleList);
+  const projectsContainer = document.querySelector('.projects-container');
+  const projectButtons = createProjectButtonList(projectTitleList);
+  projectsContainer.append(...projectButtons);
 };
 
 const createSVG = (pathDrawn) => {
@@ -23,26 +25,51 @@ const createInbox = () => {
   return createSVG(pathDrawn);
 };
 
+const createDotsHorizontal = () => {
+  const pathDrawn = 'M16,12A2,2 0 0,1 18,10A2,2 0 0,1 20,12A2,2 0 0,1 18,14A2,2 0 0,1 16,12M10,12A2,2 0 0,1 12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12M4,12A2,2 0 0,1 6,10A2,2 0 0,1 8,12A2,2 0 0,1 6,14A2,2 0 0,1 4,12Z';
+  return createSVG(pathDrawn);
+};
+
 export const createProjectButtonList = (projectList) => {
   const projectButtons = [];
 
-  projectList.forEach((project, indx) => {
-    projectButtons.push(createProjectButton(project, indx));
+  projectList.forEach((projectTitle, indx) => {
+    projectButtons.push(createProjectButton(projectTitle, indx));
   });
 
   return projectButtons;
 };
 
-export const createProjectButton = (project, indx) => {
+export const createProjectButton = (projectTitle, indx) => {
   const button = document.createElement('button');
   button.classList.add('button-container', 'project');
   button.dataset.key = indx;
   const p = document.createElement('p');
   p.classList.add('project-title');
-  p.textContent = project.title;
+  p.textContent = projectTitle;
   button.append(createInbox(), p);
 
   return button;
+};
+
+export const createProjectSection = (project) => {
+  const main = document.querySelector('main');
+  console.log(project);
+  const projectHeader = createProjectHeader(project.title, project.desc);
+
+  main.append(projectHeader);
+};
+
+const createProjectHeader = (projecTitle, projectDesc) => {
+  const div = document.createElement('div');
+  div.classList.add('project-header');
+
+  const projectTitleElem = createProjectTitle(projecTitle);
+  const projectDescElem = createprojectDesc(projectDesc);
+
+  div.append(projectTitleElem, projectDescElem);
+
+  return div;
 };
 
 const createProjectTitle = (projectTitle) => {
@@ -52,6 +79,78 @@ const createProjectTitle = (projectTitle) => {
   const h1 = document.createElement('h1');
   h1.textContent = projectTitle;
 
-  div.appendChild(h1);
+  const dropdown = createDropdown();
+
+  div.append(h1, dropdown);
+
+  return div;
+};
+
+const createDropdown = () => {
+  const div = document.createElement('div');
+  div.classList.add('dropdown');
+
+  const optionsButton = createOptionsButton();
+  const dropdownOptions = createDropdownOptions();
+
+  div.append(optionsButton, dropdownOptions);
+
+  return div;
+};
+
+const createOptionsButton = () => {
+  const button = document.createElement('button');
+  button.classList.add('options-button');
+
+  const dotsHorizontal = createDotsHorizontal();
+  button.appendChild(dotsHorizontal);
+
+  return button;
+};
+
+const createDropdownOptions = () => {
+  const div = document.createElement('div');
+  div.classList.add('dropdown-options');
+
+  const ul = document.createElement('ul');
+  const editButton = createEditButton();
+  const deleteButton = createDeleteButton();
+  
+  ul.append(editButton, deleteButton);
+  div.appendChild(ul);
+
+  return div;
+};
+
+const createDropdownListButton = (name) => {
+  const li = document.createElement('li');
+  const button = document.createElement('button');
+  const nameCapitalized = name[0].toUpperCase() + name.slice(1);
+
+  button.classList.add(name);
+  button.textContent = nameCapitalized;
+
+  li.appendChild(button);
+
+  return li;
+};
+
+const createEditButton = () => {
+  return createDropdownListButton('edit');
+};
+
+const createDeleteButton = () => {
+  return createDropdownListButton('delete');
+};
+
+const createprojectDesc = (projectDesc) => {
+  const div = document.createElement('div');
+  div.classList.add('project-desc');
+
+  const p = document.createElement('p');
+  p.textContent = projectDesc;
+
+  div.appendChild(p);
+
   return div;
 };
