eb54113d402db93ea8398ae38ce0f2b199f958c1
daniel-a-r <daniel.a-r@outlook.com>
2025-01-23 03:17:01 -0500

    refactor code

--- src/eventHandlers.js
+++ src/eventHandlers.js
@@ -2,21 +2,25 @@ import * as ls from './localStorageHandler';
 import * as htmlBuilder from './htmlBuilder';
 
 export const init = () => {
-    const projContainer = document.querySelector('.projects-container');
-    const projList = ls.getProjectList();
-    const projButtonList = htmlBuilder.createProjectButtonList(projList);
-    projContainer.append(...projButtonList);
-    // addProjectButtonEventListeners();
-    const addProjectButton = document.querySelector('.add-project');
-    addProjectButton.addEventListener('click', () => handleAddProject());
-  };
+  const addProjectButton = document.querySelector('.add-project');
+  addProjectButton.addEventListener('click', () => handleAddProject());
+  addProjectListSelectEvent();
+};
+
+const addProjectListSelectEvent = () => {
+  const projectList = document.querySelector('.projects-container');
+  for (const project of projectList.children) {
+    project.addEventListener('click', () => handleProjectSelect(project));
+  }
+}
 
 const handleAddProject = () => {
   const projContainer = document.querySelector('.projects-container');
   ls.createProject('New Project');
-  const projTitle = ls.getProjectTitles().at(-1);
-  const lastIndx = ls.getProjectList.length - 1;
+  const projTitle = ls.getLastProjectTitle();
+  const lastIndx = ls.getLastProjectIndex();
   const projectButton = htmlBuilder.createProjectButton(projTitle, lastIndx);
+  projectButton.addEventListener('click', () => handleProjectSelect(projectButton));
   projContainer.appendChild(projectButton);
 };
 
@@ -26,4 +30,6 @@ const handleProjectSelect = (newSelectedButton) => {
     prevSelectedButton.classList.toggle('selected');
   }
   newSelectedButton.classList.toggle('selected');
+  const project = ls.getProject(newSelectedButton.dataset.key);
+  htmlBuilder.createProjectSection(project);
 };
